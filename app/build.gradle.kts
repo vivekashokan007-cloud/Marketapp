1074b481bb35b4eddc0103bad2ea1f937ae7a4dc
vivekashokan007-cloud <vivekashokan007@gmail.com>
2026-04-11 13:28:00 +0530

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "2.2.0"
+        versionName = "2.2.1"
         
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
